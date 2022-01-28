542d37cb05d253c24cff8b0b6ee2d3f4b6020379
Caroline White <crwhite@bell.net>
2022-01-28 15:19:23 -0800

    Adding multi-cycle r/w and byte-wise readout

--- src/tb_vec_regfile.sv
+++ src/tb_vec_regfile.sv
@@ -6,13 +6,29 @@ parameter ADDR_WIDTH = 5;
 parameter PORTS = 2;
 
 reg clk;
-reg en [PORTS-1:0];
+reg rst;
+reg [7:0] en [PORTS-1:0];
 reg rw [PORTS-1:0];
 reg [ADDR_WIDTH-1:0] addr [PORTS-1:0];
-reg [VLEN_B-1:0] data_in [PORTS-1:0];
-reg [VLEN_B-1:0] data_out [PORTS-1:0];
+reg [DATA_WIDTH-1:0] data_in [PORTS-1:0];
+reg [DATA_WIDTH-1:0] data_out [PORTS-1:0];
+  
+  // debug signals
+  reg [DATA_WIDTH-1:0] data_in_0;
+  reg [DATA_WIDTH-1:0] data_in_1;
+  reg [DATA_WIDTH-1:0] data_out_0;
+  reg [DATA_WIDTH-1:0] data_out_1;
+  reg rw_0;
+  reg rw_1;
 
-  vec_regfile #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH)) DUT (.clk(clk), .en(en), .rw(rw), .addr(addr), .data_in(data_in), .data_out(data_out));
+  vec_regfile #(.VLEN(VLEN_B), .DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .PORTS(PORTS)) DUT (.clk(clk), .rst(rst), .en(en), .rw(rw), .addr(addr), .data_in(data_in), .data_out(data_out));
+  
+assign data_in_0 = data_in[0];
+assign data_in_1 = data_in[1];
+assign data_out_0 = data_out[0];
+assign data_out_1 = data_out[1];
+assign rw_0 = rw[0];
+assign rw_1 = rw[1];
   
 initial begin
   clk = 0;
@@ -23,34 +39,54 @@ end
 
 initial begin
   $dumpfile("dump.vcd"); $dumpvars;
-  
-  for (int i = 0; i < PORTS; i++) begin
-    addr[i] = 'b0;
-    data_in[i] = 'hABCDEF0123456789;
-    en[i] = 1'b1;
-    rw[i] = 1'b1;
+    rst = 1'b0;
+    for (int i = 0; i < PORTS; i++) begin
+      en[i] = 1'b0;
+    end
+    #20;
+    rst=1'b1;
+    
+    for (int i = 0; i < PORTS; i++) begin
+      addr[i] = 'b0;
+      data_in[i] = 'hABCDEF0123456789;
+      en[i] = 8'b01010101;
+      rw[i] = 1'b1;
+    end
     #10;
-    en[i] = 0;
+    for (int i = 0; i < PORTS; i++) begin
+      en[i] = 0;
+      data_in[i] = 'h9876543210FEDCBA;
+    end
+    #10;
+    for (int i = 0; i < PORTS; i++) begin
+      data_in[i] = 'hAAAAAAAAAAAAAAAA;
+    end
+    #10;
+    for (int i = 0; i < PORTS; i++) begin
+      data_in[i] = 'hBBBBBBBBBBBBBBBB;
+    end
+    #10;
+  
     #100;
 
+  for (int i = 0; i < PORTS; i++) begin
     assert(DUT.vec_data[addr[i]] == data_in[i])
       else $display("Data mismatch! Got %h, expected %h", data_out[i], data_in[i]);
 
-    en[i] = 1'b1;
+    en[i] = 8'b10101010;
     rw[i] = 1'b0;
-
+  end
     #10;
-
+for (int i = 0; i < PORTS; i++) begin
     en[i] = 0;
+end
 
     #20;
-
+for (int i = 0; i < PORTS; i++) begin
     assert(data_out[i] == data_in[i])
       else $display("Data mismatch! Got %h, expected %h", data_out[i], data_in[0]);
-
+end
     #100;
-  end
-  
   
   $finish;
 end
