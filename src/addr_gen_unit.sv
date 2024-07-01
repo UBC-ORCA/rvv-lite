8aeb195a37dfab734f02f981a377d4a58a77d44b
Joseph Maheshe <maheshejs@gmail.com>
2024-06-30 21:59:10 -0700

    SATURN-V: Refactor address generator unit

--- src/addr_gen_unit.sv
+++ src/addr_gen_unit.sv
@@ -1,79 +1,117 @@
-module addr_gen_unit #(
+module addr_gen_unit 
+  #(
     parameter VLEN          = 16384,
     parameter DATA_WIDTH    = 64,
     parameter ADDR_WIDTH    = 5,    // this gives us 32 vectors
     parameter OFF_WIDTH     = $clog2(VLEN/DATA_WIDTH)
-) (
-    // FIXME for avl of uneven length across regs
-    // no data reset needed, if the user picks an unused register they get garbage data and that's their problem ¯\_(ツ)_/¯
-    input                       clk,
-    input                       rst_n,
-    input                       en,
-    input                       ack,
-    input   [           1:0]    sew,
-    input   [ OFF_WIDTH-1:0]    max_off_in,
-    input   [           2:0]    max_reg_in,
-    input   [ADDR_WIDTH-1:0]    addr_in,   // register group address
-    input                       whole_reg,
-    input                       widen,
-    input   [ OFF_WIDTH-1:0]    off_in,
-    output  [ADDR_WIDTH-1:0]    addr_out, // output of v_reg address
-    output  [ OFF_WIDTH-1:0]    off_out,
-    output                      addr_start,
-    output                      addr_end,
-    output                      idle      // signal to processor that we can get another address
-);
+  ) 
+  (
     // TODO fractional lmul support would change this up
+    // no data reset needed, if the user picks an unused register they get garbage data and that's their problem ¯\_(ツ)_/¯
+    input  logic clk,
+    input  logic rst,
+    input  logic en,
+    input  logic ack,
+    input  logic [2-1:0] sew,
+    input  logic whole_reg,
+    input  logic widen_in,
+    input  logic [ OFF_WIDTH-1:0] off_in,
+    input  logic [ OFF_WIDTH-1:0] max_off_in,
+    input  logic [ADDR_WIDTH-1:0] addr_in,          // register group address
+    input  logic [ADDR_WIDTH-1:0] max_reg_in,
+    output logic [ADDR_WIDTH-1:0] addr_out,         // output of v_reg address
+    output logic [ OFF_WIDTH-1:0] off_out,
+    output logic addr_start,
+    output logic addr_end,
+    output logic idle                               // signal to processor that we can get another address
+  );
 
-    reg  [ADDR_WIDTH-1:0]   base_reg;
-    reg  [           2:0]   curr_reg, max_reg;
-    reg  [ OFF_WIDTH-1:0]   curr_off, max_off;
-    reg                     state;  // STATES: IDLE, BUSY
-    reg                     turn;   // widening
-    wire                    turn_next;
-    wire                    state_next;
-    wire [ADDR_WIDTH-1:0]   base_reg_out;
-    wire [           2:0]   curr_reg_out, max_reg_out;
-    wire [ OFF_WIDTH-1:0]   curr_off_out, max_off_out;
+  logic [ADDR_WIDTH+OFF_WIDTH-1:0] start_count_in, end_count_in;
 
-    assign addr_out         = base_reg_out + curr_reg_out;
-    assign off_out          = curr_off_out;
-    assign idle             = (widen & ~en & ~state) | (~widen & ~state_next);
+  always_comb begin
+    start_count_in = {addr_in, off_in};
+    end_count_in   = {addr_in + max_reg_in, max_off_in};
+  end
 
-    assign state_next       = rst_n & (en | (state & (curr_reg != max_reg | curr_off != max_off | (widen & ~turn))));
+  enum int unsigned {S_IDLE, S_WAIT, S_COUNT} state, nstate;
 
-    assign addr_start       = (~state | (curr_reg == max_reg & curr_off == max_off & (~widen | turn))) & en; // start of addr when en in idle state or when en while resetting
-    assign addr_end         = (state & curr_reg_out == max_reg_out & curr_off_out == max_off_out & (~widen | turn));
+  logic [ADDR_WIDTH+OFF_WIDTH-1:0] count, ncount;
+  logic [ADDR_WIDTH+OFF_WIDTH-1:0] end_count, nend_count;
+  logic widen, nwiden;
+  logic busy;
 
-    assign base_reg_out    = (addr_start ? addr_in : base_reg);
-    assign curr_reg_out    = (~addr_start & state_next) ? (curr_reg + (curr_reg != max_reg & (curr_off == max_off) & (~widen | ~turn))) : 'h0;
-    assign max_reg_out     = (addr_start ? (whole_reg ? (1 << sew) - 1 : max_reg_in) : max_reg);
+  always_comb begin
+    // defaults
+    nstate = state;
+    ncount = count;
+    nend_count = end_count;
+    nwiden = widen;
 
-    assign curr_off_out    = (~addr_start & ((state_next & ~widen) | (state & widen))) & (curr_off != max_off | widen) ? (curr_off + (curr_off != max_off & (~widen | ~turn))) : (addr_start ? off_in : 'h0);
-    assign max_off_out     = (addr_start ? (whole_reg ? (VLEN/DATA_WIDTH) - 1 : max_off_in) : max_off);
+    unique case (state)
+      S_IDLE: begin
+        if (en) begin
+          ncount = start_count_in;
+          nend_count = end_count_in;
+          nwiden = widen_in;
 
-    assign turn_next        = widen & (en | state) ? ~turn : 0;
-
-    // latching input values
-    always @(posedge clk) begin
-      if (ack) begin
-        base_reg    <= base_reg_out;
-        curr_reg    <= curr_reg_out;
-        max_reg     <= max_reg_out;
-        curr_off    <= curr_off_out;
-        max_off     <= max_off_out;
-        state       <= state_next;
-        turn        <= turn_next;
+          if (widen_in) begin
+            nstate = S_WAIT;
+          end else if (start_count_in != end_count_in) begin
+            nstate = S_COUNT;
+          end
+        end
       end
 
-      if (~rst_n) begin
-        base_reg    <= 0;
-        curr_reg    <= 0;
-        max_reg     <= 0;
-        curr_off    <= 0;
-        max_off     <= 0;
-        state       <= 0;
-        turn        <= 0;
+      S_WAIT: begin
+        nstate = S_COUNT;
+
+        if (count == end_count) begin
+          nstate = S_IDLE;
+        end
       end
+
+      S_COUNT: begin
+        ncount = count + 1;
+
+        if (widen) begin
+          nstate = S_WAIT;
+        end else if (count + 1 == end_count) begin
+          nstate = S_IDLE;
+        end
+      end
+    endcase
+  end
+
+  always_ff @(posedge clk) begin
+    if (ack) begin
+      state <= nstate;
+      count <= ncount;
+      end_count <= nend_count;
+      widen <= nwiden;
     end
+
+    if (rst) begin
+      state <= S_IDLE;
+      count <= 0;
+      end_count <= 0;
+      widen <= 0;
+    end
+  end
+
+  // Outputs
+  assign busy         = state == S_IDLE  ? en :
+                        state == S_WAIT  ? 1'b1 :
+                        state == S_COUNT ? 1'b1 :
+                        1'b0;
+  assign addr_start   = state == S_IDLE  ? en :
+                        state == S_WAIT  ? 1'b0 :
+                        state == S_COUNT ? 1'b0 :
+                        1'b0;
+  assign addr_end     = state == S_IDLE  ? en && ~widen_in && start_count_in == end_count_in :
+                        state == S_WAIT  ? count == end_count :
+                        state == S_COUNT ? ~widen && count + 1 == end_count :
+                        1'b0;
+  assign idle         = ~busy;
+  assign {addr_out, off_out} = ncount;
+
 endmodule
