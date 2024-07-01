c823efe2cde2b93e98eaa44b6c71e1ddb9434002
Joseph Maheshe <maheshejs@gmail.com>
2024-06-30 22:02:32 -0700

    SATURN-V: Refactor extract_mask and generate_be

--- src/extract_mask.sv
+++ src/extract_mask.sv
@@ -1,40 +1,57 @@
 module extract_mask 
   #(
+    parameter VLEN          = 16384,
     parameter DATA_WIDTH    = 64,
+    parameter ADDR_WIDTH    = 5,    // this gives us 32 vectors
     parameter DW_B          = DATA_WIDTH/8,
     parameter DW_B_BITS     = $clog2(DW_B),
     parameter SEW_WIDTH     = 2,
+    parameter OFF_BITS      = $clog2(VLEN/DATA_WIDTH),
     parameter ENABLE_64_BIT = 1
   )
   (
-    input  logic en,
     input  logic [SEW_WIDTH-1:0] sew,
-    input  logic [DW_B_BITS-1:0] reg_off,
-    input  logic [DW_B-1:0] vmask_in,
-    output logic [DW_B-1:0] vmask_out
+    input  logic [OFF_BITS-1:0] dw_offset,
+    input  logic mask_en,
+    input  logic [DATA_WIDTH-1:0] mask_data,
+    output logic [DW_B-1:0] mask_be
   );
 
   localparam NUM_SEWS = 1 << SEW_WIDTH;
-  logic [DW_B-1:0] vmasks[NUM_SEWS];
+  logic [DW_B-1:0] mask_bes[NUM_SEWS];
+  logic [OFF_BITS+DW_B_BITS-1:0] b;
+  logic [OFF_BITS-1:0] y;
+  logic [DW_B_BITS-1:0] x;
+  logic [DW_B-1:0] d;
 
-  genvar j, k;
+  genvar j, o;
 
   for (j = 0; j < NUM_SEWS-1; j++) begin
     localparam W_B = 1 << j;
-    for (k = 0; k < DW_B/W_B; k++) begin
-      assign vmasks[j][k*W_B +: W_B] = {W_B{vmask_in[reg_off+k]}};
+    for (o = 0; o < DW_B/W_B; o++) begin
+      always_comb begin
+        b = (OFF_BITS+DW_B_BITS)'(dw_offset) * DW_B/W_B;
+        {y,x} = (OFF_BITS+DW_B_BITS)'(b+o);
+        d = mask_data[y*DW_B +: DW_B];
+        mask_bes[j][o*W_B +: W_B] = {W_B{d[x]}};
+      end
     end
   end
 
   if (ENABLE_64_BIT) begin
     localparam W_B = 1 << (NUM_SEWS-1);
-    for (k = 0; k < DW_B/W_B; k++) begin
-      assign vmasks[NUM_SEWS-1][k*W_B +: W_B] = {W_B{vmask_in[reg_off+k]}};
+    for (o = 0; o < DW_B/W_B; o++) begin
+      always_comb begin
+        b = (OFF_BITS+DW_B_BITS)'(dw_offset) * DW_B/W_B;
+        {y,x} = (OFF_BITS+DW_B_BITS)'(b+o);
+        d = mask_data[y*DW_B +: DW_B];
+        mask_bes[NUM_SEWS-1][o*W_B +: W_B] = {W_B{d[x]}};
+      end
     end
   end else begin
-    assign vmasks[NUM_SEWS-1] = 'h0;
+    assign mask_bes[NUM_SEWS-1] = 'h0;
   end
 
-  assign vmask_out = en ? vmasks[sew] : {DW_B{1'b1}};
+  assign mask_be = mask_en ? mask_bes[sew] : {DW_B{1'b1}};
 
 endmodule
--- src/generate_be.sv
+++ src/generate_be.sv
@@ -1,6 +1,7 @@
 module generate_be 
   #(
-    parameter AVL_WIDTH     = 11,
+    parameter VLEN          = 16384,
+    parameter AVL_WIDTH     = $clog2(VLEN/8)+1,
     parameter DATA_WIDTH    = 64,
     parameter DW_B          = DATA_WIDTH/8,
     parameter DW_B_BITS     = $clog2(DW_B),
@@ -8,29 +9,36 @@ module generate_be
     parameter ENABLE_64_BIT = 1
   ) 
   (
-    input  logic [AVL_WIDTH-1:0] avl,
     input  logic [SEW_WIDTH-1:0] sew,
-    input  logic [AVL_WIDTH-1-1:0] reg_count,
+    input  logic [AVL_WIDTH-1:0] avl,
+    input  logic [AVL_WIDTH-1:0] avl_dw_offset,
     output logic [DW_B-1:0] avl_be
   );
 
   localparam NUM_SEWS = 1 << SEW_WIDTH;
   logic [DW_B-1:0] avl_bes [NUM_SEWS];
+  logic [AVL_WIDTH-1:0] b;
 
-  genvar j, k;
+  genvar j, o;
 
   // Generate mask byte enable based on SEW and current index in vector
   for (j = 0; j < NUM_SEWS-1; j++) begin
     localparam W_B = 1 << j;
-    for (k = 0; k < DW_B/W_B; k++) begin
-      assign avl_bes[j][k*W_B +: W_B] = reg_count*DW_B/W_B + k < avl ? {W_B{1'b1}} : {W_B{1'b0}};
+    for (o = 0; o < DW_B/W_B; o++) begin
+      always_comb begin
+        b = (AVL_WIDTH)'(avl_dw_offset) * DW_B/W_B;
+        avl_bes[j][o*W_B +: W_B] = (AVL_WIDTH)'(b+o) < avl ? {W_B{1'b1}} : {W_B{1'b0}};
+      end
     end
   end
 
   if (ENABLE_64_BIT) begin
     localparam W_B = 1 << (NUM_SEWS-1);
-    for (k = 0; k < DW_B/W_B; k++) begin
-      assign avl_bes[NUM_SEWS-1][k*W_B +: W_B] = reg_count*DW_B/W_B + k < avl ? {W_B{1'b1}} : {W_B{1'b0}};
+    for (o = 0; o < DW_B/W_B; o++) begin
+      always_comb begin
+        b = (AVL_WIDTH)'(avl_dw_offset) * DW_B/W_B;
+        avl_bes[NUM_SEWS-1][o*W_B +: W_B] = (AVL_WIDTH)'(b+o) < avl ? {W_B{1'b1}} : {W_B{1'b0}};
+      end
     end
   end else begin
     assign avl_bes[NUM_SEWS-1] = 'h0;
@@ -39,4 +47,3 @@ module generate_be
   assign avl_be = avl_bes[sew];
 
 endmodule
-
